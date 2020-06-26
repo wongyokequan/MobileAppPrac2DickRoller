c05a8c7d39e9166a179e18712d1c60dffa91fdad
wongyokequan <wongyokequan98@gmail.com>
2020-06-27 01:18:05 +0800

    mobile app practical 2 code challege

---
+++ src/main/java/com/example/practical1/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.practical1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+/**
+ * DiceRoller demonstrates simple interactivity in an Android app.
+ * It contains one button that updates a text view with a random
+ * value between 1 and 6.
+ */
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the Button view from the layout and assign a click
+        // listener to it.
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+    }
+
+    /**
+     * Click listener for the Roll button.
+     */
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val randomInt2 = (1..6).random()
+
+        val drawableResource2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val total = randomInt + randomInt2
+
+        diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
+        text1.text = total.toString()
+    }
+}
